f8f9ca4402f4aa052538b503b21207b62f825742
Matthew Vernacchia <mvernacc@mit.edu>
2019-02-07 15:22:25 -0500

    Add comments, fix pylint errors

--- sldbom2mcmaster.py
+++ sldbom2mcmaster.py
@@ -1,10 +1,12 @@
 """Convert Soldiworks BOM to McMaster order"""
 
+import re
 import argparse
 import pandas as pd
-import re
 
 def main(args):
+    """Load a Bill of Materials CSV, and print a string which
+    can be copied into the mcmaster.com order form."""
     data = pd.read_csv(args.sldbom)
 
     print('I loaded the following data from the SolidWorks BOM:')
@@ -14,13 +16,16 @@ def main(args):
     print('To build a McMaster order for this BOM, copy the following')
     print('into "mcmaster.com > Order > Paste products and quantities":\n')
 
-    for index, row in data.iterrows():
+    for _, row in data.iterrows():
+        # Find the McMaster part number in the file name field of the BOM.
         regex = re.compile(r'McMaster-([A-Z0-9]+)')
         matches = regex.findall(row['SW-File Name(File Name)'])
         if len(matches) == 1:
             mcmaster_part_number = matches[0]
         else:
             continue
+        # print "part number, quantity", which is the format expected by
+        # the mcmaster.com order form.
         print(mcmaster_part_number + ', {:d}'.format(row['QTY.']))
 
     print('\n\n')
@@ -31,7 +36,10 @@ def main(args):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("sldbom", help='Solidworks BOM exported as CSV file.')
-    args = parser.parse_args()
-    main(args)
+    #pylint: disable=invalid-name
+    parser = argparse.ArgumentParser(
+        description='Create McMaster orders from SolidWorks Bill of Materials.' +
+        '\nSee https://github.com/mvernacc/solidworks2mcmaster for detailed instructions.')
+    parser.add_argument("sldbom", help='SolidWorks BOM exported as CSV file.')
+    arguments = parser.parse_args()
+    main(arguments)
